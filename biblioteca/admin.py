e6e06fc836e655351a3cc443360fe8a8d6a60bfe
Lucas Bolela Silveira <lucasbolela@MacBook-Air-de-Lucas.local>
2022-06-19 18:34:58 -0300

    Add: admin painel

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from biblioteca.models import Author, Book, CreatedUpdated, Place, PublishingCompany
 
-# Register your models here.
+class CreatedUpdated(admin.ModelAdmin):
+    list_display = ('created_at', 'updated_at')
+    class Meta:
+        abstract = True
+
+class PublishingCompanies(CreatedUpdated):
+    list_display = ('name', 'state_or_city')
+
+admin.site.register(PublishingCompany, PublishingCompanies)
+
+class Authors(CreatedUpdated):
+    list_display = ('name',)
+
+admin.site.register(Author, Authors)
+
+class Places(admin.ModelAdmin):
+    list_display = ('number', 'letter')
+
+admin.site.register(Place, Places)
+
+class Books(CreatedUpdated):
+    list_display = ('title', 'publication_year', 'pages', 'subject')
+    list_per_page = 20
+
+admin.site.register(Book, Books)
